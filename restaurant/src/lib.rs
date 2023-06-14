1da8706d503575b056d7d96570a4bd5bf04d4609
michael barakat <mibaraka@microsoft.com>
2023-06-14 10:11:30 -0700

    adding some use statements

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -17,15 +17,15 @@ mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+        // fn seat_at_table() {}
     }
 
     mod serving {
-        fn take_order() {}
+        // fn take_order() {}
 
-        fn serve_order() {}
+        // fn serve_order() {}
 
-        fn take_payment() {}
+        // fn take_payment() {}
     }
 }
 
@@ -48,12 +48,14 @@ mod back_of_house {
 
     fn fix_incorrect_order() {
         cook_order();
+        // super lets us access a parent level sibling
         super::deliver_order();
     }
 
     fn cook_order() {}
 }
 
+use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     // absolute path
     // crate::front_of_house::hosting::add_to_waitlist();
@@ -65,4 +67,14 @@ pub fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please", meal.toast);
+
+    hosting::add_to_waitlist();
+}
+
+mod customer {
+    use crate::front_of_house::hosting;
+
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
 }
